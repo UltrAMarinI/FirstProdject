e0f0af237104be702c97d4b42635ab98b13ff9a9
Марина Устинина <marinaustinina@MacBook-Air-od-Marina.local>
2025-05-28 13:51:57 +0300

    PR28.05

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Urls } from '../../shared/enums/urls.enum';
-import { FormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Product } from '../../shared/interfaces/product.interface';
+
 
 @Component({
   selector: 'app-edit',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss',
   standalone: true,
@@ -15,11 +22,16 @@ export class EditComponent implements OnInit {
   isEdit = true;
 
   product: Product = {
-    name: '...',
+    name: '',
     price: 0,
   };
 
   constructor(private route: ActivatedRoute) {}
+
+  myForm: FormGroup = new FormGroup({
+    nameProduct: new FormControl('', Validators.required),
+    priceProduct: new FormControl('', Validators.required),
+  });
 
   ngOnInit(): void {
     let url = this.route.snapshot.url.join('/');
@@ -28,10 +40,18 @@ export class EditComponent implements OnInit {
   }
 
   editProduct() {
-    console.log('редактирую', this.product.name, this.product.price);
+    console.log('редактирую', this.myForm.get('nameProduct')?.value, this.myForm.get('priceProduct')?.value);
   }
 
   createProduct() {
-    console.log('создаю', this.product.name, this.product.price);
+    console.log('создаю', this.myForm.get('nameProduct')?.value, this.myForm.get('priceProduct')?.value);
+  }
+
+  submit() {
+    console.log(this.myForm);
+  }
+
+  clearForm() {
+    this.myForm.reset();
   }
 }
