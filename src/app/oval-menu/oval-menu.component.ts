4bb36fe07afb3d30ba5b099ce6a1168d25e54e22
UltraMarin <ustininamarina5@gmail.com>
2025-06-25 18:56:16 +0300

    верстка овала(вариант 1)

--- src/app/oval-menu/oval-menu.component.ts
+++ src/app/oval-menu/oval-menu.component.ts
@@ -5,23 +5,23 @@ import { List } from '../../shared/interfaces/ovalMenu.interface';
   selector: 'app-oval-menu',
   imports: [],
   templateUrl: './oval-menu.component.html',
-  styleUrl: './oval-menu.component.scss'
+  styleUrl: './oval-menu.component.scss',
 })
 export class OvalMenuComponent {
-opened = false;
-@Input() title: string | undefined;
+  opened = false;
+  @Input() title: string | undefined;
   @Input()
   array: List[] = [];
-  @Output() selectedVal=new EventEmitter<List>();
+  @Output() selectedVal = new EventEmitter<List>();
   selected!: List;
 
-  openMenu(){
-    this.opened=!this.opened;
+  openMenu() {
+    this.opened = !this.opened;
   }
 
-  select(data:List){
-    this.selected=data;
-    this.opened=false;
-    this.selectedVal.emit(data)
+  select(data: List) {
+    this.selected = data;
+    this.opened = false;
+    this.selectedVal.emit(data);
   }
 }
--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -2,16 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/service/backend.srv';
 import { Product } from '../../shared/interfaces/product.interface';
 import { RouterModule } from '@angular/router';
+import { OvalMenuComponent } from "../oval-menu/oval-menu.component";
+import { List } from '../../shared/interfaces/ovalMenu.interface';
 
 @Component({
   selector: 'app-view',
-  imports: [RouterModule],
+  imports: [RouterModule, OvalMenuComponent],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss',
   standalone: true,
 })
 export class ViewComponent implements OnInit {
   products: Product[] = [];
+  List: List[]=[
+    {value: '44', title: 'машина'},
+    {value: '55', title: 'дом'},
+    {value: '66', title: 'собака'}
+  ]
 
   constructor(private ApiService: ApiService) {}
 
@@ -30,5 +37,9 @@ export class ViewComponent implements OnInit {
     this.ApiService.deleteData(id).subscribe(() => {
       this.upDate();
     });
+  }
+
+  output($event: List){
+    console.log($event)
   }
 }
